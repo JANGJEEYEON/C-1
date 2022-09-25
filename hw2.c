1fb28d817abd6bea3edf2fe64dd3f13bbb5b8c1e
JANGJEEYEON <106160603+JANGJEEYEON@users.noreply.github.com>
2022-09-25 19:54:51 +0900

    hw2

---
+++ hw2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double km;
+	const double mile = 1.609;
+
+	printf("Please enter kilometers: ");
+	scanf("%lf", &km);
+	printf("%.1lf km is equal to %.1lf miles. \n", km, km / mile);
+
+	return 0;
+}
